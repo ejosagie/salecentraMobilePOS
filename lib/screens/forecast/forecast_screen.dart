4e0bd2cb15d1edd5e8bd24795e57c1925939db4e
Efosa JNR <efosaosagie95@gmail.com>
2026-05-31 14:15:28 +0100

    fix: import User model in forecast and price pilot screens

--- lib/screens/forecast/forecast_screen.dart
+++ lib/screens/forecast/forecast_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../models/user.dart';
 import '../../services/auth_service.dart';
 import '../../services/remote_database_service.dart';
 import '../../utils/theme.dart';
